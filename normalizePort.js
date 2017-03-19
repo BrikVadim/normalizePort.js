955a4afe7230d9a832b86a555868b1ff61e2682d
Vadim Brik <Brik99brik@gmail.com>
2017-03-19 12:48:31 +0600

    Create normalizePort.js

---
+++ normalizePort.js
@@ -0,0 +1,24 @@
+//
+// openServer System
+// Copyright © openApps. All rights reserved.
+//
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+//
+
+//-----------------------------------------------------------------------------
+
+const MIN_PORT      = 1023
+const MAX_PORT      = 65536
+
+const DEFAULT_PORT  = 8080
+
+//-----------------------------------------------------------------------------
+
+module.exports = (port, default_port = DEFAULT_PORT) => {
+    port = Number.parseInt(port)
+
+    return MIN_PORT < port && port < MAX_PORT ? port : default_port
+}
+
+//-----------------------------------------------------------------------------
